6649848a7a3e4755c4f63f2e7e1198b83eaf9ef5
ruthersmith <bercyruthersmithadvid@gmail.com>
2026-03-25 16:05:46 -0400

    from python 100

---
+++ day_061_flask_secret_advanced/login_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired("This is a required field"), Email() ])
+    password = PasswordField(label='Password')
+    submit = SubmitField(label='Log In')
+
