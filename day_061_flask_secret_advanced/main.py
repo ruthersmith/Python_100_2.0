5400ae75e76795d4f9851e2a82c8b0308243a977
ruthersmith <bercyruthersmithadvid@gmail.com>
2026-03-25 16:34:05 -0400

    added a readme and made sure that it worked

--- day_061_flask_secret_advanced/main.py
+++ day_061_flask_secret_advanced/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from login_form import LoginForm
 
+
 app = Flask(__name__)
-app.secret_key = "any-secret"
+app.secret_key = "any-secret" # This is required to use CSRF with WTForms
 
 
 @app.route("/")
@@ -13,6 +14,18 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+
+        email = loginForm.email.data
+        password = loginForm.password.data
+        
+        if email == "admin@example.com" and password == "password":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+        
+        
     return render_template('login.html', form=loginForm)
 
 
